7c9f74c0127ccf38f9ecc5484bebb98c13c766b4
WandembergViana <121878559+WandembergViana@users.noreply.github.com>
2023-01-03 11:30:31 -0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+let topBtn = document.querySelector(".sobre");
+topBtn.onclick = () => window.scrollTo({ top: 700, behavior: "smooth" });
+
+let bottomBtn = document.querySelector(".contato");
+bottomBtn.onclick = () => window.scrollTo({ top: 10000, behavior: "smooth" });
+
+let Btn2 = document.querySelector(".media");
+Btn2.onclick = () => window.scrollTo({ top: 1700, behavior: "smooth" });
